1335c69678628dc8dd42dbd2be36b8c046d7997b
Jan le Roux <jan@le-roux.co.nz>
2022-09-14 07:07:07 +1200

    .

--- Roro1/Code/Roro1_Serial1_2/Roro1_Serial1_2.ino
+++ Roro1/Code/Roro1_Serial1_2/Roro1_Serial1_2.ino
@@ -11,17 +11,18 @@
 
 void setup() {
   // Note the format for setting a serial port is as follows: Serial2.begin(baud-rate, protocol, RX pin, TX pin);
-  Serial.begin(115200);
+  //Serial.begin(115200);
   //Serial1.begin(9600, SERIAL_8N1, RXD2, TXD2);
-  Serial2.begin(9600, SERIAL_8N1, RXD2, TXD2);
-  Serial.println("Serial Txd is on pin: "+String(TX));
-  Serial.println("Serial Rxd is on pin: "+String(RX));
+  //Serial2.begin(115200, SERIAL_8N1, RXD2, TXD2);
+  Serial2.begin(115200);
+  Serial2.println("Serial Txd is on pin: "+String(TXD2));
+  Serial2.println("Serial Rxd is on pin: "+String(RXD2));
 }
 
 void loop() { //Choose Serial1 or Serial2 as required
-  while (Serial2.available()) {
-    Serial.print(char(Serial2.read()));
-  }
+  //while (Serial2.available()) {
+  //  Serial2.print(char(Serial2.read()));
+  //}
 }
 
 
